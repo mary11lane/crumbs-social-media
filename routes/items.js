7153cbe2aeda15ab6c8e80c771090a4f4c7ad8b2
mary11lane <92024647+mary11lane@users.noreply.github.com>
2023-02-04 14:34:12 +0800

    updated image storage destination

--- routes/items.js
+++ routes/items.js
@@ -13,7 +13,7 @@ import {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'frontend/public/uploads');
+    cb(null, '/public/uploads');
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
